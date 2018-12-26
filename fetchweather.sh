b3aa6d6fe180fae7db6af183d282a7b5c16e5d16
John Zastrow <br8kwall@gmail.com>
2018-12-26 14:38:37 -0500

    fixed line endings

---
+++ fetchweather.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# jcz 24-dec-18
+# fetch text dumps of local weather data
+# in the directory in which it is run
+##################################
+
+# set vx
+# Date and other variables pretty self explanatory, S is seconds
+# date format is currently YYYYMMDD_HHMMSS
+	outputter=weather_$(date +%Y-%m-%d_%H%M%S).txt
+	dater=$(date +%Y%m%d_%H%M%S)
+	dayer=$(date +%a)
+	namer=$(whoami)
+	hoster=$(hostname)
+	directory=$(pwd)
+
+echo "Date: " $dater > $outputter
+echo "Day: " $dayer >> $outputter
+echo "Computer: " $hoster >> $outputter
+echo "Directory: "$directory >> $outputter
+echo "**********************************" >> $outputter
+echo "" >> $outputter
