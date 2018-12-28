e014edbf0c3b7e28c29f0d83ce503038f26894ee
johnzastrow <br8kwall@gmail.com>
2018-12-28 12:47:19 -0800

    added new files

--- datatable.sql
+++ datatable.sql
@@ -44,7 +44,8 @@ LINES TERMINATED BY '\n'
 IGNORE 1 LINES (`dt_utc`,`pressure_mbar`,`pressure_mbar`,`temp_f`,`dewpoint_f`,`humid_perc`,`windsp_mph`,`windir_deg`,`a_press_mbar`,`a_temp_f`,`a_dewp_f`,`a_humid_perc`,`a_windsp_mph`,`a_windir_deg`
 ); 
 
-
+create database weather;
+use weather;
 CREATE TABLE `table_e1248` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `dt_utc` datetime DEFAULT NULL,
