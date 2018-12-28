8d24ff480354965dc359a0382668db813f7e6c09
John Zastrow <br8kwall@gmail.com>
2018-12-28 14:24:02 -0500

    updates

--- fetchweather.sh
+++ fetchweather.sh
@@ -14,7 +14,8 @@
 	namer=$(whoami)
 	hoster=$(hostname)
 	directory=$(pwd)
-	weather1="E1248_"$dater".csv"
+	weather1="E1248.csv"
+	weather1b="E1248_"$dater".csv"
 	loadersql = "LOAD DATA LOCAL INFILE 'C:\\Users\\jzastrow\\Documents\\GitHub\\weather\\$weather1' INTO TABLE weather.table_e1248 FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' IGNORE 1 LINES (`dt_utc`,`pressure_mbar`,`pressure_mbar`,`temp_f`,`dewpoint_f`,`humid_perc`,`windsp_mph`,`windir_deg`,`a_press_mbar`,`a_temp_f`,`a_dewp_f`,`a_humid_perc`,`a_windsp_mph`,`a_windir_deg`);"
 
 echo "Date: " $dater >> $outputter
@@ -25,8 +26,9 @@ echo "**********************************" >> $outputter
 echo "" >> $outputter
 echo "Starting $weather1"
 wget -O $weather1 -a $outputter https://weather.gladstonefamily.net/cgi-bin/wxobservations.pl?site=E1248&days=56&html=1
+cp $weather1 $weather1b
 echo "Completing $weather1"
 echo "calling mysql"
- "C:\Program Files\MariaDB 10.3\bin\mysql.exe" -uroot -pyub.miha weather $loadersql
+ "C:\\Program Files\\MariaDB 10.3\bin\mysql.exe" -uroot -pyub.miha weather $loadersql
  
  echo "done loading"
