2de7af7304f7fdf17b4155fc4bc63638ea821ca8
johnzastrow <br8kwall@gmail.com>
2018-12-31 11:37:54 -0800

    loaded data for new stations and historical data

--- import.sql
+++ import.sql
@@ -4,7 +4,7 @@ LOAD DATA LOCAL INFILE
 IGNORE INTO TABLE weather.KPWM FIELDS TERMINATED BY ',' 
 LINES TERMINATED BY '\n' IGNORE 1 LINES 
 (dt_utc,pressure_mbar,pressure_mbar,temp_f,dewpoint_f,humid_perc,
-windsp_mph,windir_deg,a_press_mbar,a_temp_f,a_dewp_f,a_humid_perc,a_windsp_mph,a_windir_d;
+windsp_mph,windir_deg,a_press_mbar,a_temp_f,a_dewp_f,a_humid_perc,a_windsp_mph,a_windir_deg);
 
 LOAD DATA LOCAL INFILE 
 '/home/jcz/Documents/github/weather/E1248large.csv' 
@@ -12,7 +12,7 @@ LOAD DATA LOCAL INFILE
 IGNORE INTO TABLE weather.E1248 FIELDS TERMINATED BY ',' 
 LINES TERMINATED BY '\n' IGNORE 1 LINES 
 (dt_utc,pressure_mbar,pressure_mbar,temp_f,dewpoint_f,humid_perc,
-windsp_mph,windir_deg,a_press_mbar,a_temp_f,a_dewp_f,a_humid_perc,a_windsp_mph,a_windir_d;
+windsp_mph,windir_deg,a_press_mbar,a_temp_f,a_dewp_f,a_humid_perc,a_windsp_mph,a_windir_deg);
 
 LOAD DATA LOCAL INFILE 
 '/home/jcz/Documents/github/weather/E4229large.csv' 
@@ -20,7 +20,7 @@ LOAD DATA LOCAL INFILE
 IGNORE INTO TABLE weather.E4229 FIELDS TERMINATED BY ',' 
 LINES TERMINATED BY '\n' IGNORE 1 LINES 
 (dt_utc,pressure_mbar,pressure_mbar,temp_f,dewpoint_f,humid_perc,
-windsp_mph,windir_deg,a_press_mbar,a_temp_f,a_dewp_f,a_humid_perc,a_windsp_mph,a_windir_d;
+windsp_mph,windir_deg,a_press_mbar,a_temp_f,a_dewp_f,a_humid_perc,a_windsp_mph,a_windir_deg);
 
 LOAD DATA LOCAL INFILE 
 '/home/jcz/Documents/github/weather/E4279large.csv' 
@@ -28,4 +28,4 @@ LOAD DATA LOCAL INFILE
 IGNORE INTO TABLE weather.E4279 FIELDS TERMINATED BY ',' 
 LINES TERMINATED BY '\n' IGNORE 1 LINES 
 (dt_utc,pressure_mbar,pressure_mbar,temp_f,dewpoint_f,humid_perc,
-windsp_mph,windir_deg,a_press_mbar,a_temp_f,a_dewp_f,a_humid_perc,a_windsp_mph,a_windir_d;
+windsp_mph,windir_deg,a_press_mbar,a_temp_f,a_dewp_f,a_humid_perc,a_windsp_mph,a_windir_deg);
