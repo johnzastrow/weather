dc593d4a9140a5d15061bffe9ed83c2dd66a738f
johnzastrow <br8kwall@gmail.com>
2018-12-31 11:17:17 -0800

    added more work

--- createdb.sql
+++ createdb.sql
@@ -1,5 +1,39 @@
-create database weather;
-use weather;
+/*
+MySQL - 10.3.11-MariaDB : Database - weather
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`weather` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `weather`;
+
+/*Table structure for table `datetest` */
+
+CREATE TABLE `datetest` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `dt_utc` datetime DEFAULT NULL,
+  `pressure_mbar` double DEFAULT NULL,
+  `ts` timestamp NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8192 DEFAULT CHARSET=utf8;
+
+
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+
+
 CREATE TABLE `E1248` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `dt_utc` datetime DEFAULT NULL,
