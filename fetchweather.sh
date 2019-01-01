f8df73b1497fc82f7c129b5dbcd7e9a04c3b3376
johnzastrow <br8kwall@gmail.com>
2019-01-01 16:05:30 -0500

    tuned up files for two new CSVs

--- fetchweather.sh
+++ fetchweather.sh
@@ -14,17 +14,22 @@
 	namer=$(whoami)
 	hoster=$(hostname)
 	directory=$(pwd)
+	
 	site1="E1248"
 	weather1="E1248.csv"
 	weather1b="E1248_"$dater".csv"
+	
 	site2="KPWM"
 	weather2="KPWM.csv"
 	weather2b="KPWM_"$dater".csv"
+	
+	site3="E4229"
 	weather3="E4229.csv"
 	weather3b="E4229_"$dater".csv"
-	weather3="E4279.csv"
-	weather3b="E4279_"$dater".csv"
-	# loadersql="LOAD DATA LOCAL INFILE '/home/jcz/Documents/github/weather/$weather1' INTO TABLE weather.table_e1248 FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' IGNORE 1 LINES (dt_utc,pressure_mbar,pressure_mbar,temp_f,dewpoint_f,humid_perc,windsp_mph,windir_deg,a_press_mbar,a_temp_f,a_dewp_f,a_humid_perc,a_windsp_mph,a_windir_deg);"
+	
+	site4="E4279"
+	weather4="E4279.csv"
+	weather4b="E4279_"$dater".csv"
 
 echo "Date: " $dater >> $outputter
 echo "Day: " $dayer >> $outputter
@@ -38,23 +43,37 @@ echo ""
 echo ""
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 
-mysql -uroot -pyub.miha weather < initial.sql
 echo "Starting Weather download"
 echo "Starting $weather1"
-wget -v -O $weather1 -a $outputter https://weather.gladstonefamily.net/cgi-bin/wxobservations.pl?site=E1248&days=56&csv=1
-echo "waiting to be done"
+wget -v -O $weather1 -a $outputter https://weather.gladstonefamily.net/cgi-bin/wxobservations.pl?site=$site1&days=56&csv=1
+echo "waiting to be done " $weather1
 wait
-echo "Copying datafile"
-cp $weather1 $weather1b
+
 echo "Completing $weather1"
 echo ""
 echo "Starting $weather2"
 wget -v -O $weather2 -a $outputter https://weather.gladstonefamily.net/cgi-bin/wxobservations.pl?site=$site2&days=56&csv=1
-echo "waiting to be done"
+echo "waiting to be done " $weather2
 wait
-echo "Copying datafile"
-cp $weather2 $weather2b
+
 echo "Completing $weather2"
+
+echo "Starting $weather3"
+wget -v -O $weather3 -a $outputter https://weather.gladstonefamily.net/cgi-bin/wxobservations.pl?site=$site3&days=56&csv=1
+echo "waiting to be done " $weather3
+wait
+
+echo "Completing $weather3"
+
+
+echo "Starting $weather4"
+wget -v -O $weather4 -a $outputter https://weather.gladstonefamily.net/cgi-bin/wxobservations.pl?site=$site4&days=56&csv=1
+echo "waiting to be done " $weather4
+wait
+
+echo "Completing $weather4"
+
+
 echo ""
 echo "calling mysql"
  mysql -uroot -pyub.miha weather < loader.sql
