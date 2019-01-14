dafae3c06a673bf0d547e2f4c788618c05aff6c9
John Zastrow <br8kwall@gmail.com>
2019-01-14 13:49:05 -0500

    Fixed insert in second column of loader

--- loader.sql
+++ loader.sql
@@ -3,7 +3,7 @@ LOAD DATA LOCAL INFILE
 -- ignore allows import to skip dupe rows
 IGNORE INTO TABLE weather.E1248 FIELDS TERMINATED BY ',' 
 LINES TERMINATED BY '\n' IGNORE 1 LINES 
-(dt_utc,pressure_mbar,pressure_mbar,temp_f,dewpoint_f,humid_perc,
+(dt_utc,pressure_mbar,temp_f,dewpoint_f,humid_perc,
 windsp_mph,windir_deg,a_press_mbar,a_temp_f,a_dewp_f,a_humid_perc,a_windsp_mph,a_windir_deg);
 
 LOAD DATA LOCAL INFILE 
@@ -11,7 +11,7 @@ LOAD DATA LOCAL INFILE
 -- ignore allows import to skip dupe rows
 IGNORE INTO TABLE weather.KPWM FIELDS TERMINATED BY ',' 
 LINES TERMINATED BY '\n' IGNORE 1 LINES 
-(dt_utc,pressure_mbar,pressure_mbar,temp_f,dewpoint_f,humid_perc,
+(dt_utc,pressure_mbar,temp_f,dewpoint_f,humid_perc,
 windsp_mph,windir_deg,a_press_mbar,a_temp_f,a_dewp_f,a_humid_perc,a_windsp_mph,a_windir_deg);
 
 LOAD DATA LOCAL INFILE
@@ -19,7 +19,7 @@ LOAD DATA LOCAL INFILE
 -- ignore allows import to skip dupe rows
 IGNORE INTO TABLE weather.E4279 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n' IGNORE 1 LINES
-(dt_utc,pressure_mbar,pressure_mbar,temp_f,dewpoint_f,humid_perc,
+(dt_utc,pressure_mbar,temp_f,dewpoint_f,humid_perc,
 windsp_mph,windir_deg,a_press_mbar,a_temp_f,a_dewp_f,a_humid_perc,a_windsp_mph,a_windir_deg);
 
 LOAD DATA LOCAL INFILE
@@ -27,5 +27,5 @@ LOAD DATA LOCAL INFILE
 -- ignore allows import to skip dupe rows
 IGNORE INTO TABLE weather.E4229 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n' IGNORE 1 LINES
-(dt_utc,pressure_mbar,pressure_mbar,temp_f,dewpoint_f,humid_perc,
+(dt_utc,pressure_mbar,temp_f,dewpoint_f,humid_perc,
 windsp_mph,windir_deg,a_press_mbar,a_temp_f,a_dewp_f,a_humid_perc,a_windsp_mph,a_windir_deg);
