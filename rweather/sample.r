3914986bd554ea7cbfea4113f93c803406df52db
johnzastrow <br8kwall@gmail.com>
2019-02-06 06:29:10 -0800

    Cleaned up the file

--- rweather/sample.r
+++ rweather/sample.r
@@ -5,8 +5,8 @@ library(DBI)
 
 
 # R needs a full path to find the settings file.
-rmariadb.settingsfile<-"F:\\br8k_home\\GitHub\\weather\\my.cnf"
-
+# rmariadb.settingsfile<-"F:\\br8k_home\\GitHub\\weather\\my.cnf"
+rmariadb.settingsfile<-"/home/jcz/Documents/github/weather/my.cnf"
 
 rmariadb.db<-"weather"
 weather<-dbConnect(RMariaDB::MariaDB(),default.file=rmariadb.settingsfile,group=rmariadb.db) 
@@ -17,29 +17,14 @@ dbListTables(weather)
 # disconnect to clean up the connection to the database.
 dbDisconnect(weather)
 
-localuserpassword <- "yub.miha"
-library(RMariaDB)
-# The connection method below uses a password stored in a variable.  
-# To use this set localuserpassword="The password of newspaper_search_results_user" 
-
-weather <- dbConnect(RMariaDB::MariaDB(), user='jcz', password=localuserpassword, dbname='weather', host='192.168.1.110')
-dbListTables(weather)
-dbDisconnect(weather)
-
-dbListTables(con)
-dbWriteTable(con, "mtcars", mtcars)
-dbListTables(con)
-
-dbListFields(con, "mtcars")
-dbReadTable(con, "mtcars")
 
 # You can fetch all results:
-res <- dbSendQuery(con, "SELECT * FROM mtcars WHERE cyl = 4")
+res <- dbSendQuery(weather, "SELECT * FROM E4229_daily")
 dbFetch(res)
 dbClearResult(res)
 
 # Or a chunk at a time
-res <- dbSendQuery(con, "SELECT * FROM mtcars WHERE cyl = 4")
+res <- dbSendQuery(weather, "SELECT * FROM E4229_daily")
 while(!dbHasCompleted(res)){
   chunk <- dbFetch(res, n = 5)
   print(nrow(chunk))
