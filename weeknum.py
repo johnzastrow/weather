d4357358716c1284d47752c6e46f9572aa0b3c5c
John Zastrow <br8kwall@gmail.com>
2019-10-09 12:58:47 -0400

    charts for all stations

---
+++ weeknum.py
@@ -0,0 +1,12 @@
+import datetime
+today = (datetime.datetime.now().strftime("%Y, %M, %d"))
+week = (datetime.datetime.now().strftime("%W"))
+print(today)
+print(week)
+
+
+week1 = datetime.date(2010, 6, 22).isocalendar()[1]
+# week2 = datetime.date(today).isocalendar()[1]
+
+print(week1)
+# print(week2)
