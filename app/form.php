6c6711c564f7b2629692d06263befedc73c2f801
John Zastrow <br8kwall@gmail.com>
2020-05-13 11:42:49 -0400

    typo

--- app/form.php
+++ app/form.php
@@ -6,16 +6,16 @@ error_reporting(E_ALL);
 
 require_once 'classes/user.php';
 
-$objUser = new User();
+$objuser = new user();
 // GET
 if(isset($_GET['edit_id'])){
     $id = $_GET['edit_id'];
-    $stmt = $objUser->runQuery("SELECT * FROM notes WHERE id=:id");
+    $stmt = $objuser->runQuery("SELECT * FROM notes WHERE id=:id");
     $stmt->execute(array(":id" => $id));
-    $rowUser = $stmt->fetch(PDO::FETCH_ASSOC);
+    $rowuser = $stmt->fetch(PDO::FETCH_ASSOC);
 }else{
   $id = null;
-  $rowUser = null;
+  $rowuser = null;
 }
 
 // POST
@@ -25,14 +25,14 @@ if(isset($_POST['btn_save'])){
 
   try{
      if($id != null){
-       if($objUser->update($comment, $type, $id)){
-         $objUser->redirect('index.php?updated');
+       if($objuser->update($comment, $type, $id)){
+         $objuser->redirect('index.php?updated');
        }
      }else{
-       if($objUser->insert($comment, $type)){
-         $objUser->redirect('index.php?inserted');
+       if($objuser->insert($comment, $type)){
+         $objuser->redirect('index.php?inserted');
        }else{
-         $objUser->redirect('index.php?error');
+         $objuser->redirect('index.php?error');
        }
      }
   }catch(PDOException $e){
@@ -55,20 +55,20 @@ if(isset($_POST['btn_save'])){
                 <!-- Sidebar menu -->
                 <?php require_once 'includes/sidebar.php'; ?>
                 <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
-                  <h1 style="margin-top: 10px">Add / Edit Users</h1>
+                  <h1 style="margin-top: 10px">Add / Edit users</h1>
                   <p>Required fields are in (*)</p>
                   <form  method="post">
                     <div class="form-group">
                         <label for="id">ID</label>
-                        <input class="form-control" type="text" comment="id" id="id" value="<?php print($rowUser['id']); ?>" readonly>
+                        <input class="form-control" type="text" comment="id" id="id" value="<?php print($rowuser['id']); ?>" readonly>
                     </div>
                     <div class="form-group">
                         <label for="comment">comment *</label>
-                        <input  class="form-control" type="text" comment="comment" id="comment" placeholder="First comment and Last comment" value="<?php print($rowUser['comment']); ?>" required maxlength="100">
+                        <input  class="form-control" type="text" comment="comment" id="comment" placeholder="First comment and Last comment" value="<?php print($rowuser['comment']); ?>" required maxlength="100">
                     </div>
                     <div class="form-group">
                         <label for="type">Type *</label>
-                        <input  class="form-control" type="text" comment="type" id="type" placeholder="johndoel@gmail.com" value="<?php print($rowUser['type']); ?>" required maxlength="100">
+                        <input  class="form-control" type="text" comment="type" id="type" placeholder="johndoel@gmail.com" value="<?php print($rowuser['type']); ?>" required maxlength="100">
                     </div>
                     <input class="btn btn-primary mb-2" type="submit" comment="btn_save" value="Save">
                   </form>
